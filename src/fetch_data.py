c7aef60073efccc97ab6db524d4799cb1ea3dd78
aj-sc <alberth0804@gmail.com>
2026-01-13 20:13:51 -0500

    Base extract script created

--- src/fetch_data.py
+++ src/fetch_data.py
@@ -10,7 +10,7 @@ BASE_URL = "https://www.googleapis.com/youtube/v3"
 API_KEY = os.getenv("API_KEY")
 CHANNEL_ID = os.getenv("CHANNEL_ID")
 
-def get_video_ids():
+def get_video_id_string() -> str:
     request_url = f"{BASE_URL}/search"
     
     request_params = {
@@ -22,20 +22,48 @@ def get_video_ids():
     }
 
     response = requests.get(request_url, params=request_params)
-
-    return response.json()
-
     
+    response_data = response.json().get("items", {})
 
-def get_video_data():
+    video_ids = [item.get("id", {}).get("videoId", "") for item in response_data]
+    
+    id_string = ",".join(video_ids)
+
+    return id_string
+
+def get_video_data(id_string: str):
     request_url = f"{BASE_URL}/videos"
 
-    request_params = {}
+    request_params = {
+        "key" : API_KEY,
+        "id" : id_string,
+        "part" : "snippet, contentDetails, statistics, topicDetails"
+    }
 
     response = requests.get(request_url, params=request_params)
+    
+    response_data = response.json().get("items", [])
 
-    pass
+    video_data = []
 
-data = get_video_ids()
+    for video in response_data:
+        video_data.append(
+            {
+                "video_id" : video.get("id", ""),
+                "video_title" : video.get("snippet", {}).get("title", ""),
+                "published_at" : video.get("snippet", {}).get("publishedAt", ""),
+                "views" : video.get("statistics", {}).get("viewCount", ""),
+                "likes" : video.get("statistics", {}).get("likeCount", ""),
+                "comments" : video.get("statistics", {}).get("commentCount", ""),
+                "favorites" : video.get("statistics", {}).get("favoriteCount", ""),
+                "topics" : video.get("topicDetails", {}).get("topicCategories", [])
+            }
+        )
 
-print(data)
+    return video_data
+    
+
+id_string = get_video_id_string()
+video_data = get_video_data(id_string)
+
+print(video_data)
