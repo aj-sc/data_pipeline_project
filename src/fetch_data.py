bad3a1addc52bfa7aee9e976b5af2e120cebd6e1
aj-sc <alberth0804@gmail.com>
2026-01-14 11:15:10 -0500

    Python script finished

--- src/fetch_data.py
+++ src/fetch_data.py
@@ -1,6 +1,6 @@
 import os
 import requests
-import json
+import polars as pl
 from datetime import datetime
 from dotenv import load_dotenv
 
@@ -10,60 +10,95 @@ BASE_URL = "https://www.googleapis.com/youtube/v3"
 API_KEY = os.getenv("API_KEY")
 CHANNEL_ID = os.getenv("CHANNEL_ID")
 
-def get_video_id_string() -> str:
+def get_video_id_data() -> str:
     request_url = f"{BASE_URL}/search"
     
-    request_params = {
-        "key" : API_KEY,
-        'channelId' : CHANNEL_ID,
-        'part' : 'snippet',
-        'maxResults' : 50,
-        'order' : 'date',
-    }
+    video_ids = []
+    page_token = ''
 
-    response = requests.get(request_url, params=request_params)
-    
-    response_data = response.json().get("items", {})
+    while True:    
+        request_params = {
+            "key" : API_KEY,
+            "channelId" : CHANNEL_ID,
+            "part" : "snippet",
+            "maxResults" : 50,
+            "order" : "date",
+            "pageToken" : ""
+        }   
 
-    video_ids = [item.get("id", {}).get("videoId", "") for item in response_data]
-    
-    id_string = ",".join(video_ids)
+        if page_token:
+            request_params["pageToken"] = page_token
 
-    return id_string
+        try:
+            response = requests.get(request_url, params=request_params)
+            response.raise_for_status()
 
-def get_video_data(id_string: str):
+            response_data = response.json()
+
+            items = response_data.get("items", [])
+
+            video_ids.extend([item.get("id", {}).get("videoId", "") for item in items])
+
+            page_token = response_data.get("nextPageToken", "")
+
+            if not page_token:
+                break
+
+        except requests.exceptions.HTTPError as err_h:
+            print(f"HTTP error: {err_h}")
+        except requests.exceptions.RequestException as err_r:
+            print(f"Request error: {err_r}")
+
+    return video_ids
+
+def get_video_data(video_id_list: list) -> list:
     request_url = f"{BASE_URL}/videos"
-
-    request_params = {
-        "key" : API_KEY,
-        "id" : id_string,
-        "part" : "snippet, contentDetails, statistics, topicDetails"
-    }
-
-    response = requests.get(request_url, params=request_params)
     
-    response_data = response.json().get("items", [])
-
     video_data = []
 
-    for video in response_data:
-        video_data.append(
-            {
-                "video_id" : video.get("id", ""),
-                "video_title" : video.get("snippet", {}).get("title", ""),
-                "published_at" : video.get("snippet", {}).get("publishedAt", ""),
-                "views" : video.get("statistics", {}).get("viewCount", ""),
-                "likes" : video.get("statistics", {}).get("likeCount", ""),
-                "comments" : video.get("statistics", {}).get("commentCount", ""),
-                "favorites" : video.get("statistics", {}).get("favoriteCount", ""),
-                "topics" : video.get("topicDetails", {}).get("topicCategories", [])
-            }
-        )
+    for i in range(0, len(video_id_list), 50):
+        video_batch = video_id_list[i:i+50]
+        id_string = ",".join(video_batch)
+
+
+        request_params = {
+            "key" : API_KEY,
+            "id" : id_string,
+            "part" : "snippet, contentDetails, statistics, topicDetails"
+        }
+
+        try:
+            response = requests.get(request_url, params=request_params)
+            response.raise_for_status()
+
+            response_data = response.json().get("items", [])
+
+            for video in response_data:
+                video_data.append(
+                    {
+                        "video_id" : video.get("id", ""),
+                        "video_title" : video.get("snippet", {}).get("title", ""),
+                        "published_at" : video.get("snippet", {}).get("publishedAt", ""),
+                        "views" : video.get("statistics", {}).get("viewCount", ""),
+                        "likes" : video.get("statistics", {}).get("likeCount", ""),
+                        "comments" : video.get("statistics", {}).get("commentCount", ""),
+                        "favorites" : video.get("statistics", {}).get("favoriteCount", ""),
+                        "topics" : video.get("topicDetails", {}).get("topicCategories", [])
+                    }
+                )
+        except requests.exceptions.HTTPError as err_h:
+            print(f"HTTP Error: {err_h}")
 
     return video_data
+
+def main() -> None:
+    video_id_data = get_video_id_data()
+    video_data = get_video_data(video_id_data)
     
+    run_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-id_string = get_video_id_string()
-video_data = get_video_data(id_string)
+    df = pl.DataFrame(video_data)
+    df.write_parquet(f"{run_time}-data.parquet")
 
-print(video_data)
+if __name__ == "__main__":
+    main() 
